d206ecf7c6fb0cb922f1f18bb88f1f45c41c14c0
nirbhay244 <kumarnirbhay844@gmail.com>
2023-07-12 21:35:36 +0530

    mock2 q1

---
+++ Mock2Q1/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const box = document.getElementById("box");
+  
+    box.addEventListener("click", function() {
+      if (box.style.backgroundColor === "red") {
+        box.style.backgroundColor = "yellow";
+        box.style.width = "600px";
+        box.style.height = "600px";
+      } else {
+        box.style.backgroundColor = "red";
+        box.style.width = "300px";
+        box.style.height = "300px";
+      }
+    });
+  });
+  
